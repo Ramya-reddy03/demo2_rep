fb4abb4df4ff057c0b00c4af15bda8375eacab32
ramya <ramyareddie.03@gmail.com>
2025-03-25 12:27:08 +0530

    files commites

--- hello.java
+++ hello.java
@@ -1,7 +1,13 @@
 class hello
 {
+<<<<<<< HEAD
 public static void main(String[]args)
 {
 System.out.println("hello");
+=======
+public static void main(String args[])
+{
+System.out.println("welcome to vbit");
+>>>>>>> e22648f (files commited)
 }
 }
